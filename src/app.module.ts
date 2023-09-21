d1954c79fe1fb2807b8623abd91b5688e0f26531
gomboczmarton <gomboczmarton@gmail.com>
2023-09-21 14:37:47 +0200

    Grid model, endpoint, calculator service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GridModule } from './grid/grid.module';
 
 @Module({
-  imports: [],
+  imports: [GridModule],
   controllers: [AppController],
   providers: [AppService],
 })
